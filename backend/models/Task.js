f54d0d0cc54c7ec6407a6dd384d07b1d2f4daee4
shriyashshingare <shriyashshingare@gmail.com>
2020-10-04 16:59:04 +0530

    initial task and workspace api is done

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require('mongodb')
 const mongo = require('./Model')
 class Task {
 
@@ -5,25 +6,66 @@ class Task {
         this.task = 'task'
     }
 
-    task = async (bodyInfo) => {
-        try {
-            let task = {
-                title: bodyInfo.title,
-                blob: bodyInfo.JSONObject,
-                deadline: bodyInfo.deadline,
-                status: bodyInfo.status,
-                creationUserID: bodyInfo.creationUserID,
-                status: bodyInfo.status,
-                userIDs: bodyInfo.userIDs
+    taskAction = async (bodyInfo) => {
+        if (bodyInfo.action == 1) {  // create task
+            try {
+                let taskData = bodyInfo.taskData;
+                let task = {
+                    title: taskData.title,
+                    blob: taskData.JSONObject,
+                    deadline: taskData.deadline,
+                    status: taskData.status,
+                    createdDate: new Date(),
+                    lastModified: new Date(),
+                    lastModifiedUser:taskData.lastModifiedUser,
+                    creationUserID: bodyInfo.creationUserID,
+                    status: bodyInfo.status,
+                    userIDs: bodyInfo.userIDs
+                }
+                let insert = await mongo.usacrm.collection(this.task).insertOne(task)
+                return { 'success': true, 'message': "Task is created successfully" }
+
+            } catch (error) {
+                return { 'success': false, 'message': "Task is created Un-successfully" };
             }
-            let insert = await mongo.usacrm.collection(this.task).insertOne(task)
-            return { 'success': true, 'status': 200, 'message': "Task is created successfully" }
+        } else if (bodyInfo.action == 2) { // update task
+            try {
+                let taskId = bodyInfo.taskId;
+                let updatedTaskData = bodyInfo.updatedTaskData;
+                let updatedTaskDataKeys = Object.keys(updatedTaskData);
+                let taskData = await mongo.usacrm.collection(this.task).findOne({ _id: new ObjectId(taskId) }).toArray();
+                for (let i = 0; i < updatedTaskDataKeys.length; i++) {
+                    taskData[updatedTaskDataKeys[i]] = updatedTaskData[i];
+                }
+                let updateResult = await mongo.usacrm.collection(this.task).replaceOne({ _id: new ObjectId(taskId) }, taskData)
+                return { 'success': true, 'message': "Task is updated successfully" }
+            } catch (error) {
+                return { 'success': false, 'message': "Task is updated Un-successfully" };
 
-        } catch (error) {
-            return { 'success': false, 'status': 400, 'message': "Task is created Un-successfully" };
+            }
+        } else if (bodyInfo.action == 3) {  //delete task
+            let taskId = bodyInfo.taskId;
+            let deleteFilter = [
+                {
+                    "$match":
+                    {
+                        "_id": ObjectId(taskId)
+                    }
+                },
+                {
+                    "$project":
+                    {
+                        "title": 1,
+                        "taskId": '$_id',
+                        "_id":0
+                    }
+                }
+            ]
+            let deletedTaskData = await mongo.usacrm.collection(this.task).aggregate(deleteFilter).toArray()
+            let deleteResult = deletedTaskData[0]
+            deleteResult['status']=4
+            let updateResult = await mongo.usacrm.collection(this.task).replaceOne({ _id: new ObjectId(taskId) }, deleteResult)
         }
-
-
     }
 }
 
