fde9ea0403fa27eae552615ce7095e41c23e97c8
NerdioN <neel99khalade@gmail.com>
2020-10-06 15:39:49 +0530

    authorization request from headers, jwt verification in coding standards

--- backend/controllers/OrganizationController.js
+++ backend/controllers/OrganizationController.js
@@ -6,14 +6,12 @@ module.exports.manage = async (req, res) => {
     try {
         const oragnization = new Organization()
 
-        let legitUser = await new User().verifyUser(req.body.token, res)
-        let updatedName = req.body.updatedName
-        if(legitUser) {
-            if(req.body.method == 0) res.json(await oragnization.getMyOrganization(legitUser._id))
-            if(req.body.method == 1) res.json(await oragnization.createOrganization(req.body.orgName, legitUser))
-            if(req.body.method == 2) res.json(await oragnization.updateOrganizationName(legitUser, updatedName))
-
-        } else res.json({success:false, message:''})
+        let legitUser = await new User().verifyUser(request.headers.authorization.token)
+        if(legitUser.success) {
+            if(req.body.method == 0) res.json(await oragnization.getMyOrganization(legitUser.message._id))
+            if(req.body.method == 1) res.json(await oragnization.createOrganization(req.body.orgName, legitUser.message))
+            if(req.body.method == 2) res.json(await oragnization.updateOrganizationName(legitUser.message, req.body.updatedName))
+        } else res.json(legitUser)
     } catch (e) {
         res.send({ "Success": false, "Error": e.toString(), "Payload": [] });
     }
