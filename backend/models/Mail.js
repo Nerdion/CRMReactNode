a2d412a284dcd302aa13ccc657a253bd7fcb7b1c
NerdioN <neel99khalade@gmail.com>
2020-10-08 11:36:32 +0530

    resolved sending of mail

--- backend/models/Mail.js
+++ backend/models/Mail.js
@@ -10,18 +10,8 @@ class Mail {
             pass: 'wdsurjybgdcdypct',
         }
         this.host = 'smtp.mail.yahoo.com'
-        this.service = 'yahoo',
-            this.port = 465,
-
-            this.transporter = nodemailer.createTransport({
-                host: this.host,
-                service: this.service,
-                secure: false,
-                port: this.port,
-                auth: this.auth,
-                debug: true,
-                logger: true
-            });
+        this.service = 'yahoo'
+        this.port = 465
     }
 
     sendMail = (options) => {
@@ -32,36 +22,24 @@ class Mail {
             subject: options.subject,
             text: options.text,
         };
-        let isSend = true
-        let promise = new Promise(function (resolve, reject) {
-            this.transporter.sendMail(mailOptions, (error, info) => {
-                if (error) {
-                    isSend = false
-                    console.log(error)
-                }
-                resolve(info);
-            });
-        });
 
-        return promise;
+        return new Promise((resolve,reject)=> {
+            this.transporter = nodemailer.createTransport({
+                host: this.host,
+                service: this.service,
+                secure: false,
+                port: this.port,
+                auth: this.auth,
+                debug: true,
+                logger: true
+            });
+
+            this.transporter.sendMail(mailOptions, function(error, info){
+                if (error) resolve(false);
+                else resolve(true);
+            })
+        })
     }
 }
 
 module.exports = Mail
-
-async function wrapedSendMail(mailOptions){
-    return new Promise((resolve,reject)=>{
-    let transporter = nodemailer.createTransport({//settings});
-
- transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-        console.log("error is "+error);
-       resolve(false); // or use rejcet(false) but then you will have to handle errors
-    } 
-   else {
-       console.log('Email sent: ' + info.response);
-       resolve(true);
-    }
-   });
-   }
-  
