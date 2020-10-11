282178b97019c60de72d4b54976b69ecd5f64520
Dontag <nishadsur@gmail.com>
2020-10-11 18:48:36 +0530

    1. Added user invite in createtask page. 2. Added Avatar in Workspaces.

--- frontend/src/views/CRM_Apis.js
+++ frontend/src/views/CRM_Apis.js
@@ -14,4 +14,8 @@ export const AuthUser = `${DomainURL}/api/authorizeuser`;
 
 export const AuthUserResponse = `${DomainURL}/api/authrizeresponsedata`;
 
-export const VerifyEmailUser = `${DomainURL}/api/authorizeRegistereduser`
+export const VerifyEmailUser = `${DomainURL}/api/authorizeRegistereduser`;
+
+export const getWorkSpaceApi = `${DomainURL}/api/getWorkSpace`;
+
+export const setWorkSpaceApi = `${DomainURL}/api/setWorkSpace`;
